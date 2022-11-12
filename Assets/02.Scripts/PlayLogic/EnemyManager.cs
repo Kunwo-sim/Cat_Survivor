145d669422fac95051764b28821e53367f72462b
Nakuri <ccmuk58@naver.com>
2022-11-13 07:42:31 +0900

    [Fix] EnemyManager 간단한 버그 픽스
    + 클래스명과 파일명이 다른 문제 수정
    + 네임스페이스 추가
    + 수정 필요한 사항 주석 추가

--- Assets/02.Scripts/PlayLogic/EnemyManager.cs
+++ Assets/02.Scripts/PlayLogic/EnemyManager.cs
@@ -1,38 +1,43 @@
-using System;
 using PlayLogic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class EnemySpawner : MonoBehaviour
+namespace PlayLogic
 {
-    private Transform _playerTransform;
-
-    private void Awake()
+    public class EnemyManager : MonoBehaviour
     {
-        _playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
-    }
+        private Transform _playerTransform;
 
-    private void Start()
-    {
-        InvokeRepeating(nameof(SpawnEnemy), 0f, 0.5f);
-        InvokeRepeating(nameof(SpawnEnemy), 0f, 0.5f);
-    }
+        private void Awake()
+        {
+            _playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        }
 
-    private void SpawnEnemy()
-    {
-        GameObject enemy = ObjectPoolManager.GetObject(EPoolObjectType.Enemy1);
-        enemy.transform.position = GetRandomPosition();
-    }
+        private void Start()
+        {
+            InvokeRepeating(nameof(SpawnEnemy), 0f, 0.5f);
+            InvokeRepeating(nameof(SpawnEnemy), 0f, 0.5f);
+        }
+
+        private void SpawnEnemy()
+        {
+            // Enemy ScriptableObject화에 따른 확장 구조화 필요
+            GameObject enemy = ObjectPoolManager.GetObject(EPoolObjectType.Enemy1);
+            enemy.transform.position = GetRandomPosition();
+        }
     
-    public Vector3 GetRandomPosition()
-    {
-        Vector3 playerPosition = _playerTransform.position;
-        float radius = Random.Range(23f, 25f);
-        float x = Random.Range(-radius + playerPosition.x, radius + playerPosition.x);
-        float y = Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(x - playerPosition.x, 2)) + playerPosition.y;
-        y *= Random.Range(0, 2) == 0 ? -1 : 1;
+        public Vector3 GetRandomPosition()
+        {
+            // 생성 위치 관련 수정 필요
+            float radius = Random.Range(23f, 25f);
+            
+            Vector3 playerPosition = _playerTransform.position;
+            float x = Random.Range(-radius + playerPosition.x, radius + playerPosition.x);
+            float y = Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(x - playerPosition.x, 2)) + playerPosition.y;
+            y *= Random.Range(0, 2) == 0 ? -1 : 1;
  
-        Vector3 randomPosition = new Vector3(x, y, 0);
-        return randomPosition;
+            Vector3 randomPosition = new Vector3(x, y, 0);
+            return randomPosition;
+        }
     }
 }
