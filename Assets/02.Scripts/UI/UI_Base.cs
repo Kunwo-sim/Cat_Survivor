1a7219dd1655f22c16e8262c620018b94184df3b
SKW <rjsdntlsdj@gmail.com>
2022-11-14 21:42:43 +0900

    [Add] UI_Manager 초안 구현

---
+++ Assets/02.Scripts/UI/UI_Base.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class UI_Base : MonoBehaviour
+{
+    protected Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    public abstract void Init();
+
+    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (typeof(T) == typeof(GameObject))
+                objects[i] = Util.FindChild(gameObject, names[i], true);
+            else
+            {
+                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+            }
+            if (objects[i] == null)
+                Debug.Log($"Failed to bind({names[i]})");
+        }
+    }
+
+    protected T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+        if (_objects.TryGetValue(typeof(T), out objects) == false)
+            return null;
+        return objects[idx] as T;
+    }
+
+    protected GameObject GetObject(int idx)
+    {
+        return Get<GameObject>(idx);
+    }
+    protected Text GetText(int idx)
+    {
+        return Get<Text>(idx);
+    }
+
+    protected Button GetButton(int idx)
+    {
+        return Get<Button>(idx);
+    }
+
+    protected Image GetImage(int idx)
+    {
+        return Get<Image>(idx);
+    }
+
+    public static void BindEvent(GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+
+        UI_EventHandler evt = Util.GetOrAddComponent<UI_EventHandler>(go);
+
+        switch (type)
+        {
+            case Define.UIEvent.Click:
+                evt.OnClickHandler -= action;
+                evt.OnClickHandler += action;
+                break;
+            case Define.UIEvent.Drag:
+                evt.OnDragHandler -= action;
+                evt.OnDragHandler += action;
+                break;
+        }
+
+        if (evt == null)
+            evt = go.AddComponent<UI_EventHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
+
+    }
+}
---
+++ Assets/02.Scripts/UI/UI_EventHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_EventHandler : MonoBehaviour, IDragHandler, IPointerClickHandler
+{
+    public Action<PointerEventData> OnClickHandler = null;
+    public Action<PointerEventData> OnDragHandler = null;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (OnClickHandler != null)
+            OnClickHandler.Invoke(eventData);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        if (OnDragHandler != null)
+        {
+            OnDragHandler.Invoke(eventData);
+        }
+    }
+}
