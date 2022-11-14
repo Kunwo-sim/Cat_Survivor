a534c75b764d987d33888ce612778eb5d0ea1e74
SKW <rjsdntlsdj@gmail.com>
2022-11-14 21:35:22 +0900

    [Add] Define 추가

---
+++ Assets/02.Scripts/Utils/Define.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum Scene
+    {
+        Login,
+        Lobby,
+        Game,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
+    }
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+    public enum MouseEvent
+    {
+        Press,
+        PointDown,
+        PointUp,
+        Click,
+    }
+    public enum CamerMode
+    {
+        QuaterView,
+    }
+}
