d2dd0836287d703194253798b549b9bb6f544782
Nakuri <ccmuk58@naver.com>
2022-11-21 05:23:21 +0900

    [FIx] Projectile 작은 수정

--- Assets/02.Scripts/PlayLogic/Skills/Projectile.cs
+++ Assets/02.Scripts/PlayLogic/Skills/Projectile.cs
@@ -26,6 +26,7 @@ public abstract class Projectile : MonoBehaviour
 
     private void Delete()
     {
+        StopCoroutine(Move());
         ObjectPoolManager.ReturnObject(gameObject, _poolType);
     }
 
