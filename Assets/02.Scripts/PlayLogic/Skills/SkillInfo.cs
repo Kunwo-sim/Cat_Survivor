b524350e7e721e707c8e83fb1fa683c2aab06a85
Nakuri <ccmuk58@naver.com>
2022-11-20 21:39:05 +0900

    [Fix] 스킬 시스템 구조 변경
    + 스크립터블오브젝트를 하나로 병합

--- Assets/02.Scripts/PlayLogic/Skills/SkillInfo.cs
+++ Assets/02.Scripts/PlayLogic/Skills/SkillInfo.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 namespace PlayLogic
 {
-    public abstract class SkillInfo : ScriptableObject
+    [CreateAssetMenu(fileName = "Skill1", menuName = "Skills/Skill1")]
+    public class SkillInfo : ScriptableObject
     {
         [SerializeField] private new string name = "New Skill";
         [SerializeField] protected int damage = 1;
@@ -15,6 +16,28 @@ namespace PlayLogic
         [field: SerializeField] public float BaseCoolDown { get; set; } = 1;
         public float NextCoolDown { get; set; } = 0;
 
-        public abstract void Activate(Transform holderTrans);
+        public void Activate(Transform holderTrans)
+        {
+            // 스킬 생성 위치 설정 (무기)
+            Transform spawnTransform = holderTrans;
+            
+            // 오브젝트 생성
+            Projectile cloneProjectile = ObjectPoolManager.GetObject(poolType).GetComponent<Projectile>();
+            cloneProjectile.Initialize(spawnTransform.position, spawnTransform.rotation, damage, activeTime, poolType);
+        }
+        
+        // public override void Activate(Transform holderTrans)
+        // {
+        //     // 스킬 생성 위치 설정 (무기)
+        //     Vector2 spawnPos = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-6.0f, 6.0f));
+        //     
+        //     // 스킬 방향 및 힘 설정
+        //     float rot = Mathf.Atan2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * Mathf.Rad2Deg;
+        //     Quaternion spawnRot = Quaternion.Euler(0, 0, rot - 90);
+        //     
+        //     // 오브젝트 생성
+        //     Projectile cloneProjectile = ObjectPoolManager.GetObject(poolType).GetComponent<Projectile>();
+        //     cloneProjectile.Initialize(spawnPos, spawnRot, damage, activeTime, poolType);
+        // }
     }
 }
