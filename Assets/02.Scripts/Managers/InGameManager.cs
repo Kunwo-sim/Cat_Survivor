5aa391e2b07c011a1034dc5a73b64be78bc9c55d
Nakuri <ccmuk58@naver.com>
2022-11-21 05:00:55 +0900

    [Add] InGameManager 추가
    + 일단은 타이머 기능을 위한 클래스

---
+++ Assets/02.Scripts/Managers/InGameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+    private float _time;
+    private readonly float _endTime = 60 * 5;
+    private Bar _timeBar;
+    private void Awake()
+    {
+        _timeBar = GameObject.Find("Time Bar").GetComponent<Bar>();
+    }
+    private void Start()
+    {
+        _time = 0;
+        _timeBar.SetBar(_endTime, _time);
+    }
+    private void Update()
+    {
+        SetTime();
+    }
+
+    private void SetTime()
+    {
+        _time += Time.deltaTime;
+        _timeBar.SetBar(_endTime, _time);
+    }
+}
