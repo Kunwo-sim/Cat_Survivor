3d5cce986c4eb1490a9cf40bbf4ba2b92a36c658
Nakuri <ccmuk58@naver.com>
2022-11-24 08:25:24 +0900

    [Add] 일부 스프라이트 적용 및 문제 수정
    + 일부 스프라이트 적용
    + 임시 배경 적용
    + 일부 방향에서 스킬이 거꾸로 나가는 문제 해결(임시)

--- Assets/02.Scripts/PlayLogic/Skills/Projectiles/TestProjectile1.cs
+++ Assets/02.Scripts/PlayLogic/Skills/Projectiles/TestProjectile1.cs
@@ -6,8 +6,16 @@ public class TestProjectile1 : Projectile
     protected override IEnumerator Move()
     {
         Speed = 20;
-        Vector2 force = transform.up.normalized * Speed;
+        transform.rotation *= Quaternion.Euler(new Vector3(0,0,90));
+        Vector2 force = transform.right.normalized * Speed;
         Rigidbody2D.AddForce(force, ForceMode2D.Impulse);
+
+        // Test
+        if (transform.eulerAngles.z is > 90 and <= 270)
+            GetComponent<SpriteRenderer>().flipY = true;
+        else
+            GetComponent<SpriteRenderer>().flipY = false;
+        
         yield return null;
     }
 }
