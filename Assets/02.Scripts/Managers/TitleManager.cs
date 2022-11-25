b844af0cdfceaa01eafd544b28bc5259a63fa992
SKW <rjsdntlsdj@gmail.com>
2022-11-26 04:13:21 +0900

    [타이틀] 애니메이션 적용, 씬로드 적용

--- Assets/02.Scripts/Managers/TitleManager.cs
+++ Assets/02.Scripts/Managers/TitleManager.cs
@@ -8,33 +8,35 @@ using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
-    public TextMeshProUGUI Text_Title;
+    public Image Logo;
     public Button Button_Start;
 
 
     void Start()
     {
         Application.targetFrameRate = 120;
-        Text_Title = GameObject.Find("Text_Title").GetComponent<TextMeshProUGUI>();
+        Logo = GameObject.Find("Logo").GetComponent<Image>();
         Button_Start = GameObject.Find("Button_Start").GetComponent<Button>();
         Button_Start.onClick.AddListener(Button_Start_Clicked);
 
-        TitleFadeEffect();
-        ButtonPadeEffect();
-        DontDestroyOnLoad(this.gameObject);
-    }
+        Sequence LogoSequence = DOTween.Sequence().SetAutoKill(false).OnStart(() =>
+        {
+            Logo.transform.localScale = Vector3.zero;
+            Logo.GetComponent<CanvasGroup>().alpha = 0;
+        }).Append(Logo.transform.DOScale(1, 1).SetEase(Ease.OutBounce)).Join(GetComponent<CanvasGroup>().DOFade(1, 1)).SetDelay(0.5f);
 
-    public void TitleFadeEffect()
-    {
-        Text_Title.DOFade(0.0f, 1.0f).SetLoops(-1, LoopType.Yoyo);
+        LogoSequence.Play();
+        Invoke("ButtonPadeEffect", 0.5f);
+        DontDestroyOnLoad(this.gameObject);
     }
 
     public void ButtonPadeEffect()
     {
+        Button_Start.gameObject.SetActive(true);
         Button_Start.GetComponent<Image>().DOFade(0.0f, 1.0f).SetLoops(-1, LoopType.Yoyo);
     }
     public void Button_Start_Clicked()
     {
-        SceneManager.LoadScene("Lobby");
+        CatSceneManager.Instance.ChangeScene("Lobby");
     }
 }
