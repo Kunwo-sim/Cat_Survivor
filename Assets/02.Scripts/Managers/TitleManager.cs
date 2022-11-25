dd274fb2bb2703f83cf6788e8bfc80e7ad8cf2a5
SKW <rjsdntlsdj@gmail.com>
2022-11-25 19:41:00 +0900

    [타이틀] Dotween 적용 테스트

--- Assets/02.Scripts/Managers/TitleManager.cs
+++ Assets/02.Scripts/Managers/TitleManager.cs
@@ -2,11 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
+    public TextMeshProUGUI Text_Title;
+    public Button Button_Start;
     public void Button_Start_Clicked()
     {
         SceneManager.LoadScene("Lobby");
+    }
+
+    void Start()
+    {
+        Text_Title = GameObject.Find("Text_Title").GetComponent<TextMeshProUGUI>();
+        Button_Start = GameObject.Find("Button_Start").GetComponent<Button>();
+        TitleFadeEffect();
+        ButtonPadeEffect();
+    }
+
+    public void TitleFadeEffect()
+    {
+        Text_Title.DOFade(0.0f, 1.0f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void ButtonPadeEffect()
+    {
+        Button_Start.GetComponent<Image>().DOFade(0.0f, 1.0f).SetLoops(-1, LoopType.Yoyo);
     }
 }
