0d432001b1b6f33a3a2a51ac12b64181260d3247
Nakuri <ccmuk58@naver.com>
2023-01-10 03:59:06 +0900

    [Fix] CSVReader.cs MonoBehaviour 상속 제거

--- Assets/02.Scripts/Utils/CSVReader.cs
+++ Assets/02.Scripts/Utils/CSVReader.cs
@@ -3,43 +3,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
- 
-public class CSVReader : MonoBehaviour
+
+public static class CSVReader
 {
     static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
     static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
-    static char[] TRIM_CHARS = { '\"' };
+    static char[] TRIM_CHARS = {'\"'};
 
     public static List<Dictionary<string, object>> Read(string file)
     {
         var list = new List<Dictionary<string, object>>();
-        TextAsset data = Resources.Load (file) as TextAsset;
- 
-        var lines = Regex.Split (data.text, LINE_SPLIT_RE);
- 
-        if(lines.Length <= 1) return list;
- 
+        TextAsset data = Resources.Load(file) as TextAsset;
+
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
         var header = Regex.Split(lines[0], SPLIT_RE);
-        for(var i=1; i < lines.Length; i++) {
- 
+        for (var i = 1; i < lines.Length; i++)
+        {
             var values = Regex.Split(lines[i], SPLIT_RE);
-            if(values.Length == 0 ||values[0] == "") continue;
- 
+            if (values.Length == 0 || values[0] == "") continue;
+
             var entry = new Dictionary<string, object>();
-            for(var j=0; j < header.Length && j < values.Length; j++ ) {
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
                 string value = values[j];
                 value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
                 object finalvalue = value;
                 int n;
                 float f;
-                if(int.TryParse(value, out n)) {
+                if (int.TryParse(value, out n))
+                {
                     finalvalue = n;
-                } else if (float.TryParse(value, out f)) {
+                }
+                else if (float.TryParse(value, out f))
+                {
                     finalvalue = f;
                 }
                 entry[header[j]] = finalvalue;
             }
-            list.Add (entry);
+            list.Add(entry);
         }
         return list;
     }
