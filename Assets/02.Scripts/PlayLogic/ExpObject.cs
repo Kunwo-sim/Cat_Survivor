3d8e806723baa53e50031cc96ffd03f476134233
SKW <rjsdntlsdj@gmail.com>
2023-01-19 02:23:05 +0900

    [Exp] Exp 먹는 로직 변경

--- Assets/02.Scripts/PlayLogic/ExpObject.cs
+++ Assets/02.Scripts/PlayLogic/ExpObject.cs
@@ -6,10 +6,13 @@ public class ExpObject : MonoBehaviour
     private Player _player;
     private float _exp;
     private readonly EPoolObjectType _poolType = EPoolObjectType.ExpObject;
-    private Tweener _backToPlayer;
+    private bool _bIsAcquire = false;
+    private float _moveSpeed = 0.0f;
 
     private void Awake()
     {
+        _bIsAcquire = false;
+        _moveSpeed = 0.0f;
         _renderer = GetComponent<SpriteRenderer>();
         _player = GameObject.FindWithTag("Player").GetComponent<Player>();
     }
@@ -17,8 +20,7 @@ public class ExpObject : MonoBehaviour
     public void Initialize(float exp, Vector3 position)
     {
         _exp = exp;
-        float size = _exp * 0.02f + 0.3f;
-        transform.localScale = new Vector3(size, size, 1);
+
         if (_exp < 2)
         {
             _renderer.color = Color.white;
@@ -31,28 +33,41 @@ public class ExpObject : MonoBehaviour
         {
             _renderer.color = Color.yellow;
         }
+
         transform.position = position;
+    }
+
+    void Update()
+    {
+        if (_bIsAcquire)
+        {
+            Vector3 dir = new Vector3(_player.transform.position.x - transform.position.x, _player.transform.position.y - transform.position.y, 0);
+            dir = dir.normalized;
+            transform.position += (dir * _moveSpeed);
+            _moveSpeed += 0.001f;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            Sequence sequence = DOTween.Sequence();
-            Vector2 curPos = transform.position;
-            Vector2 moveOffset = _player.PjoyStick.JoyDirection * 10;
-            Vector3 playerPos = _player.transform.position;
+            ObjectPoolManager.ReturnObject(gameObject, _poolType);
+            _player.ReceiveExp(_exp);
 
-            sequence.Append(transform.DOMove(curPos + moveOffset, 0.5f).SetEase(Ease.OutQuad))
-                .Append(transform.DOMove(playerPos, 0.5f).SetEase(Ease.InQuad))
-                .OnComplete(ExpCallback);
-            sequence.Play();
+            //Sequence sequence = DOTween.Sequence();
+            //Vector2 curPos = transform.position;
+            //Vector2 moveOffset = _player.PjoyStick.JoyDirection * 10;
+            //Vector3 playerPos = _player.transform.position;
+
+            //sequence.Append(transform.DOMove(curPos + moveOffset, 0.5f).SetEase(Ease.OutQuad))
+            //    .Append(transform.DOMove(playerPos, 0.5f).SetEase(Ease.InQuad))
+            //    .OnComplete(ExpCallback);
+            //sequence.Play();
         }
-    }
-
-    public void ExpCallback()
-    {
-        ObjectPoolManager.ReturnObject(gameObject, _poolType);
-        _player.ReceiveExp(_exp);
+        else if (col.CompareTag("PlayerMagnet"))
+        {
+            _bIsAcquire = true;
+        }
     }
 }
