c77bf347e0be099e0e834872efbd2bf45aeffe95
SKW <rjsdntlsdj@gmail.com>
2023-01-18 18:19:59 +0900

    [매니저] 인게임매니저 수정 및 인게임에 웨이브매니저 연동

--- Assets/02.Scripts/Managers/InGameManager.cs
+++ Assets/02.Scripts/Managers/InGameManager.cs
@@ -6,9 +6,8 @@ using UnityEngine.UI;
 
 public class InGameManager : MonoBehaviour
 {
-    [SerializeField]
-    TextMeshProUGUI _timeText;
-    float WaveRemainTime = 5.0f;
+    
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +18,6 @@ public class InGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        WaveRemainTime -= Time.deltaTime;
-        _timeText.text = "남은 시간 : " + (int)WaveRemainTime;
 
-        if (WaveRemainTime < 0.0f)
-        {
-            WaveRemainTime = 5.0f;
-            Time.timeScale = 0.0f;
-            UIManager.Instance.ShowPopupUI<UI_WaveShop>("UI_WaveShop");
-        }
     }
 }
