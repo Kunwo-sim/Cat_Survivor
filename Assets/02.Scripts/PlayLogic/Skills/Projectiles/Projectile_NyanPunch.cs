4b66e6e61f68516726e83a1bf92da6284394404c
Nakuri <ccmuk58@naver.com>
2023-01-18 17:31:34 +0900

    [스킬] 임시 냥펀치 테스트 코드

--- Assets/02.Scripts/PlayLogic/Skills/Projectiles/Projectile_NyanPunch.cs
+++ Assets/02.Scripts/PlayLogic/Skills/Projectiles/Projectile_NyanPunch.cs
@@ -9,7 +9,14 @@ public class Projectile_NyanPunch : Projectile
     {
         yield return null;
     }
-    
+
+    public override void Initialize(Vector2 spawnPos, Quaternion spawnRot, int damage, float activeTime, EPoolObjectType poolType)
+    {
+        base.Initialize(spawnPos, spawnRot, damage, activeTime, poolType);
+        // Test
+        gameObject.GetComponentsInChildren<Transform>()[1].localPosition += new Vector3(Random.Range(-0.7f, 0.7f), 0, 0);
+    }
+
     protected override void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Enemy"))
