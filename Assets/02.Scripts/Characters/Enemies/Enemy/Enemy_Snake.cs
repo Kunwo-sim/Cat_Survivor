30b12955b27dd8f5956d545d8a34187d56f47348
Nakuri <ccmuk58@naver.com>
2023-01-27 06:07:59 +0900

    [적] 뱀 추가

---
+++ Assets/02.Scripts/Characters/Enemies/Enemy/Enemy_Snake.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class Enemy_Snake : Enemy
+{
+    protected override void Routine()
+    {
+        int random = Random.Range(0, 1);
+        switch (random)
+        {
+            case 0:
+                StartCoroutine(Routine_Move());
+                break;
+        }
+    }
+}
