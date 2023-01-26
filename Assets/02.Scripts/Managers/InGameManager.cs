428abe7da6ebbff080ea9961e5aa9c0b1370c1f3
SKW <rjsdntlsdj@gmail.com>
2023-01-27 03:48:25 +0900

    [해상도] 인게임 해상도 대응

--- Assets/02.Scripts/Managers/InGameManager.cs
+++ Assets/02.Scripts/Managers/InGameManager.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 
 public class InGameManager : MonoBehaviour
 {
-    
-    
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        int setWidth = 1280; // 사용자 설정 너비
+        int setHeight = 720; // 사용자 설정 높이
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+        
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+        
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
     }
 }
